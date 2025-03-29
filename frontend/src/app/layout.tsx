74a66f082ce5537ff582c6a8fb0c05f804625855
Nish323 <doragon.nini@icloud.com>
2025-03-30 01:58:19 +0900

    認証機能の追加

--- frontend/src/app/layout.tsx
+++ frontend/src/app/layout.tsx
@@ -1,6 +1,11 @@
 import type { Metadata } from "next";
-import { Geist, Geist_Mono } from "next/font/google";
+import { Geist, Geist_Mono, Inter } from "next/font/google";
 import "./globals.css";
+import { AuthProvider } from '@/utils/contexts/AuthContext';
+import { Loading } from "@/components/molecules/admin/Loading";
+
+
+const inter = Inter({ subsets: ["latin"] });
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -24,10 +29,11 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
-      >
-        {children}
+      <body className={inter.className}>
+        <AuthProvider>
+          <Loading />
+          {children}
+        </AuthProvider>
       </body>
     </html>
   );
