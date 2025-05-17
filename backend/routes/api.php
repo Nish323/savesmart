5d968c1f9a424f12f8f80aa71cec09c796427e41
Nish323 <doragon.nini@icloud.com>
2025-05-17 16:27:35 +0900

    expensesテーブルのデータ送信

--- backend/routes/api.php
+++ backend/routes/api.php
@@ -3,6 +3,7 @@
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\UserController;
+use App\Http\Controllers\ExpenseController;
 
 Route::get('/', function () {
     return redirect('client/login');
@@ -17,4 +18,5 @@ Route::middleware(['auth:sanctum', 'ability:user'])->group(function () {
     Route::get('/user/check-auth', [UserController::class, 'checkAuth']);
     Route::post('/user/logout', [UserController::class, 'logout']);
     Route::get('/user/top', [UserController::class, 'showTop']);
+    Route::get('/expenses', [ExpenseController::class, 'index']);
 });
